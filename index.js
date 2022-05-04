ee00c3050f52a994510eb26f9222e61321570ceb
ippolito <lin.ippolito@gmail.com>
2022-05-04 19:14:54 +0200

    Initialisation du projet

---
+++ index.js
@@ -0,0 +1,8 @@
+// On recupere le paquet express et on le stock dans la variable express
+const express = require('express')
+
+// On crée un application express
+const  app = express()
+
+app.listen(3000, () => console.log("Api Piiquante port 3000"))
+
